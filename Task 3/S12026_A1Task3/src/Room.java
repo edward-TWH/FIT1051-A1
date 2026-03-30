bf86506a342497adc2de1b69a1d51dc4c57406cf
Edward Tan <edward_tan.git@pm.me>
2026-03-30 17:25:38 +1100

    Add getters and setters for Room attributes

--- Task 3/S12026_A1Task3/src/Room.java
+++ Task 3/S12026_A1Task3/src/Room.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+public class Room
+{
+    private int id;
+    private int[] exits;
+    private Monster monster;
 
-public class Room {
+    public Room()
+    {
+        this.id = 0;
+        this.exits = new int[4];
+        this.monster = new Monster();
+    }
+
+    public int[] getExits()
+    {
+        return this.exits;
+    }
+
+    public int getId()
+    {
+        return this.id;
+    }
+
+    public Monster getMonster()
+    {
+        return this.monster;
+    }
+
+    public void setExits(int[] newExits)
+    {
+        this.exits = newExits;
+    }
+
+    public void setId(int newId)
+    {
+        this.id = newId;
+    }
+
+    public void setMonster(Monster newMonster)
+    {
+        this.monster = newMonster;
+    }
 }
